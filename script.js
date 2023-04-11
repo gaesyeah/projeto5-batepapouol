5aacfc037253fcf87c03599ba6fbe3dfe96ec617
gaesyeah <gabrielgoesrios@gmail.com>
2023-04-11 16:55:33 -0300

    ajustes finos e login transformado em função

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
-const user_name = prompt('Qual o seu nome');
+//VARIAVEIS GLOBAIS
+let message, user_name;
+//--------------------------
 
-let message = document.querySelector('.messages_container');
-message.innerHTML += `
-    <div class="message_box ENTERorEXIT">
-        <p class="time">(00:00:00)</p>
-        <p class="message"><strong>${user_name}</strong> entra na sala...</p>
-    </div>
-`
+function login() {
 
+    user_name = prompt('Qual o seu nome');
+
+    message = document.querySelector('.messages_container');
+    message.innerHTML += `
+        <div class="message_box ENTERorEXIT">
+            <p class="time">(00:00:00)</p>
+            <p class="message"><strong>${user_name}</strong> entra na sala...</p>
+        </div>
+    `
+}
+
+login();
+//-------------------------------------------------------
 function send(send){
 
     let input = document.querySelector('.input_message');
-    let inputM = input.value;
+    let input_send = input.value;
 
-    if (inputM == "") {
+    if (input_send == "") {
         return; //para não fazer nada caso o usuario envie uma mensagem "vazia"
     }
 
     message.innerHTML += `
         <div class="message_box">
             <p class="time">(00:00:00)</p>
-            <p class="message"><strong>${user_name}</strong> para <strong>todos</strong>: ${inputM}</p>
+            <p class="message"><strong>${user_name}</strong> para <strong>todos</strong>: ${input_send}</p>
         </div>
     `
     document.querySelector('.input_message').value = "";
 }
+//----------------------------------------------------------
